97cba5522616daae5ed1ad27662a5c6b6a322d6a
Tanwi Mallick <tanwimallick@gmail.com>
2021-08-02 16:52:39 -0500

    first commit

---
+++ model/pytorch/dcrnn_supervisor.py
@@ -0,0 +1,271 @@
+import os
+import time
+
+import numpy as np
+import torch
+from torch.utils.tensorboard import SummaryWriter
+
+from lib import utils
+from model.pytorch.dcrnn_model import DCRNNModel
+from model.pytorch.loss import masked_mae_loss
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+class DCRNNSupervisor:
+    def __init__(self, adj_mx, **kwargs):
+        self._kwargs = kwargs
+        self.base_dir =  kwargs.get('base_dir')
+        self.batch_size = kwargs.get('batch_size')
+        self.dataset_dir = kwargs.get('dataset_dir')
+        self.test_batch_size = kwargs.get('test_batch_size')
+        self.val_batch_size = kwargs.get('val_batch_size')
+        self.graph_pkl_filename = kwargs.get('graph_pkl_filename')
+
+        self.cl_decay_steps = kwargs.get('cl_decay_steps')
+        self.filter_type = kwargs.get('filter_type')
+        self.horizon = kwargs.get('horizon')
+        self.input_dim = kwargs.get('input_dim')
+        self.l1_decay = kwargs.get('l1_decay')
+        self.max_diffusion_step = kwargs.get('max_diffusion_step')
+        self.num_nodes = kwargs.get('num_nodes')
+        self.num_rnn_layers = kwargs.get('num_rnn_layers')
+        self.output_dim = kwargs.get('output_dim')
+        self.rnn_units = kwargs.get('rnn_units')
+        self.seq_len = kwargs.get('seq_len')
+        self.use_curriculum_learning = kwargs.get('use_curriculum_learning')
+
+        self.base_lr = kwargs.get('base_lr')
+        self.dropout = kwargs.get('dropout')
+        self._epoch_num = kwargs.get('epoch')
+        self.epochs = kwargs.get('epochs')
+        self.max_grad_norm = kwargs.get('max_grad_norm')
+        self.steps = kwargs.get('steps')
+
+        self._log_dir = self.base_dir
+        self._writer = SummaryWriter('runs/' + self._log_dir)
+        log_level = self._kwargs.get('log_level', 'INFO')
+        self._logger = utils.get_logger(self._log_dir, __name__, 'info.log', level=log_level)
+
+        # data set
+        print(self.dataset_dir)
+        self._data = utils.load_dataset(self.dataset_dir, self.batch_size, self.test_batch_size)
+        self.standard_scaler = self._data['scaler']
+
+        
+        # setup model
+        dcrnn_model = DCRNNModel(adj_mx) #, self._logger) , **self._model_kwargs)
+        self.dcrnn_model = dcrnn_model.cuda() if torch.cuda.is_available() else dcrnn_model
+        self._logger.info("Model created")
+
+        # self._epoch_num = self._train_kwargs.get('epoch', 0)
+        # if self._epoch_num > 0:
+        #     self.load_model()
+
+
+
+    def save_model(self, epoch):
+        if not os.path.exists('models/'):
+            os.makedirs('models/')
+
+        config = dict(self._kwargs)
+        config['model_state_dict'] = self.dcrnn_model.state_dict()
+        config['epoch'] = epoch
+        torch.save(config, 'models/epo%d.tar' % epoch)
+        self._logger.info("Saved model at {}".format(epoch))
+        return 'models/epo%d.tar' % epoch
+
+    def load_model(self):
+        self._setup_graph()
+        assert os.path.exists('models/epo%d.tar' % self._epoch_num), 'Weights at epoch %d not found' % self._epoch_num
+        checkpoint = torch.load('models/epo%d.tar' % self._epoch_num, map_location='cpu')
+        self.dcrnn_model.load_state_dict(checkpoint['model_state_dict'])
+        self._logger.info("Loaded model at {}".format(self._epoch_num))
+
+    def _setup_graph(self):
+        with torch.no_grad():
+            self.dcrnn_model = self.dcrnn_model.eval()
+
+            val_iterator = self._data['val_loader'].get_iterator()
+
+            for _, (x, y) in enumerate(val_iterator):
+                x, y = self._prepare_data(x, y)
+                output = self.dcrnn_model(x)
+                break
+
+    
+
+    def evaluate(self, dataset='val', batches_seen=0):
+        """
+        Computes mean L1Loss
+        :return: mean L1Loss
+        """
+        with torch.no_grad():
+            self.dcrnn_model = self.dcrnn_model.eval()
+
+            val_iterator = self._data['{}_loader'.format(dataset)].get_iterator()
+            losses = []
+
+            y_truths = []
+            y_preds = []
+
+            for _, (x, y) in enumerate(val_iterator):
+                x, y = self._prepare_data(x, y)
+
+                output = self.dcrnn_model(x)
+                loss = self._compute_loss(y, output)
+                losses.append(loss.item())
+
+                y_truths.append(y.cpu())
+                y_preds.append(output.cpu())
+
+            mean_loss = np.mean(losses)
+
+            self._writer.add_scalar('{} loss'.format(dataset), mean_loss, batches_seen)
+
+            y_preds = np.concatenate(y_preds, axis=1)
+            y_truths = np.concatenate(y_truths, axis=1)  # concatenate on batch dimension
+
+            y_truths_scaled = []
+            y_preds_scaled = []
+            for t in range(y_preds.shape[0]):
+                y_truth = self.standard_scaler.inverse_transform(y_truths[t])
+                y_pred = self.standard_scaler.inverse_transform(y_preds[t])
+                y_truths_scaled.append(y_truth)
+                y_preds_scaled.append(y_pred)
+
+            return mean_loss, {'prediction': y_preds_scaled, 'truth': y_truths_scaled}
+
+
+
+    def train(self, patience=50, epochs=100, lr_decay_ratio=0.1, log_every=1, save_model=1,
+               test_every_n_epochs=10, epsilon=1e-8): #, **kwargs):
+        # steps is used in learning rate - will see if need to use it?
+        min_val_loss = float('inf')
+        wait = 0
+        optimizer = torch.optim.Adam(self.dcrnn_model.parameters(), lr=self.base_lr, eps=epsilon)
+
+        lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=self.steps,
+                                                            gamma=lr_decay_ratio)
+
+        self._logger.info('Start training ...')
+
+        # this will fail if model is loaded with a changed batch_size
+        num_batches = self._data['train_loader'].num_batch
+        self._logger.info("num_batches:{}".format(num_batches))
+
+        batches_seen = num_batches * self._epoch_num
+
+        for epoch_num in range(self._epoch_num, self.epochs):
+
+            self.dcrnn_model = self.dcrnn_model.train()
+
+            train_iterator = self._data['train_loader'].get_iterator()
+            losses = []
+
+            start_time = time.time()
+
+            for _, (x, y) in enumerate(train_iterator):
+                optimizer.zero_grad()
+
+                x, y = self._prepare_data(x, y)
+
+                output = self.dcrnn_model(x, y, batches_seen)
+
+                if batches_seen == 0:
+                    # this is a workaround to accommodate dynamically registered parameters in DCGRUCell
+                    optimizer = torch.optim.Adam(self.dcrnn_model.parameters(), lr=self.base_lr, eps=epsilon)
+
+                loss = self._compute_loss(y, output)
+
+                self._logger.debug(loss.item())
+
+                losses.append(loss.item())
+
+                batches_seen += 1
+                loss.backward()
+
+                # gradient clipping - this does it in place
+                torch.nn.utils.clip_grad_norm_(self.dcrnn_model.parameters(), self.max_grad_norm)
+
+                optimizer.step()
+            self._logger.info("epoch complete")
+            lr_scheduler.step()
+            self._logger.info("evaluating now!")
+
+            val_loss, _ = self.evaluate(dataset='val', batches_seen=batches_seen)
+
+            end_time = time.time()
+
+            self._writer.add_scalar('training loss',
+                                    np.mean(losses),
+                                    batches_seen)
+
+            if (epoch_num % log_every) == log_every - 1:
+                message = 'Epoch [{}/{}] ({}) train_mae: {:.4f}, val_mae: {:.4f}, lr: {:.6f}, ' \
+                          '{:.1f}s'.format(epoch_num, epochs, batches_seen,
+                                           np.mean(losses), val_loss, lr_scheduler.get_lr()[0],
+                                           (end_time - start_time))
+                self._logger.info(message)
+
+            if (epoch_num % test_every_n_epochs) == test_every_n_epochs - 1:
+                test_loss, _ = self.evaluate(dataset='test', batches_seen=batches_seen)
+                message = 'Epoch [{}/{}] ({}) train_mae: {:.4f}, test_mae: {:.4f},  lr: {:.6f}, ' \
+                          '{:.1f}s'.format(epoch_num, epochs, batches_seen,
+                                           np.mean(losses), test_loss, lr_scheduler.get_lr()[0],
+                                           (end_time - start_time))
+                self._logger.info(message)
+
+            if val_loss < min_val_loss:
+                wait = 0
+                if save_model:
+                    model_file_name = self.save_model(epoch_num)
+                    self._logger.info(
+                        'Val loss decrease from {:.4f} to {:.4f}, '
+                        'saving to {}'.format(min_val_loss, val_loss, model_file_name))
+                min_val_loss = val_loss
+
+            elif val_loss >= min_val_loss:
+                wait += 1
+                if wait == patience:
+                    self._logger.warning('Early stopping at epoch: %d' % epoch_num)
+                    break
+
+    def _prepare_data(self, x, y):
+        x, y = self._get_x_y(x, y)
+        x, y = self._get_x_y_in_correct_dims(x, y)
+        return x.to(device), y.to(device)
+
+    def _get_x_y(self, x, y):
+        """
+        :param x: shape (batch_size, seq_len, num_sensor, input_dim)
+        :param y: shape (batch_size, horizon, num_sensor, input_dim)
+        :returns x shape (seq_len, batch_size, num_sensor, input_dim)
+                 y shape (horizon, batch_size, num_sensor, input_dim)
+        """
+        x = torch.from_numpy(x).float()
+        y = torch.from_numpy(y).float()
+        self._logger.debug("X: {}".format(x.size()))
+        self._logger.debug("y: {}".format(y.size()))
+        x = x.permute(1, 0, 2, 3)
+        y = y.permute(1, 0, 2, 3)
+        return x, y
+
+    def _get_x_y_in_correct_dims(self, x, y):
+        """
+        :param x: shape (seq_len, batch_size, num_sensor, input_dim)
+        :param y: shape (horizon, batch_size, num_sensor, input_dim)
+        :return: x: shape (seq_len, batch_size, num_sensor * input_dim)
+                 y: shape (horizon, batch_size, num_sensor * output_dim)
+        """
+        batch_size = x.size(1)
+        x = x.view(self.seq_len, batch_size, self.num_nodes * self.input_dim)
+        y = y[..., :self.output_dim].view(self.horizon, batch_size,
+                                          self.num_nodes * self.output_dim)
+        return x, y
+
+    def _compute_loss(self, y_true, y_predicted):
+        loss = torch.nn.L1Loss()
+        # y_true = self.standard_scaler.inverse_transform(y_true)
+        # y_predicted = self.standard_scaler.inverse_transform(y_predicted)
+        return loss(y_predicted, y_true)
